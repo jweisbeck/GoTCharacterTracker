26a183dc7f3dd13deff4741d954531bda61155ab
Jesse Weisbeck <jesse.weisbeck@buildium.com>
2017-11-16 20:37:44 -0500

    Initial commit

---
+++ GoTCharacterTracker.Data/Services/CharacterService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GoTCharacterTracker.Data.Characters.Services
+{
+    public class Character
+    {
+        // private members
+        protected  List<CharacterDTO> m_allCharacters;
+
+        public Character(List<CharacterDTO> names)
+        {
+            foreach (var name in names)
+            {
+                m_allCharacters.Add(name);
+            }
+        }
+
+
+        public IEnumerable<CharacterDTO> GetCharactersByFamily(string family)
+        {
+            var characters = m_allCharacters.Where(c => c.Family == family);
+            return characters;
+        }
+
+
+        public class CharacterDTO
+        {
+            public string Name { get; set; }
+            public bool IsAlive { get; set; }
+            public string Family { get; set; } // make Family an enum & maybe a class later
+        }
+    }
+}
